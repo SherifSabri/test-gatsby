a357aee10d85e96fb432d81e1e76f83fe37cf082
sherif <sherifsabri.io@hotmail.com>
2020-06-25 16:37:31 -0700

    added github workflow

--- src/components/layout.js
+++ src/components/layout.js
@@ -54,6 +54,7 @@ const Layout = ({ location, title, children }) => {
         marginRight: `auto`,
         maxWidth: rhythm(24),
         padding: `${rhythm(1.5)} ${rhythm(3 / 4)}`,
+        minHeight: `calc(100vh - 11rem)`
       }}
     >
       <header>{header}</header>
